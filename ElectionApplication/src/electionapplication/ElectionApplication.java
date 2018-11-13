cb8d21d10d5b92ee2dfb19dfdaaf3bc401d06a7e
17138311 <35848872+17138311@users.noreply.github.com>
2018-11-13 21:41:36 +0000

    mark 13.11.18 GUI

--- ElectionApplication/src/electionapplication/ElectionApplication.java
+++ ElectionApplication/src/electionapplication/ElectionApplication.java
@@ -15,6 +15,9 @@ public class ElectionApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        PollGui myGUI = new PollGui();
+        myGUI.setVisible(true);
+        
         // TODO code application logic here
     }
     
