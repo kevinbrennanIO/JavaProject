2bec5a6433c942308ec4f5663f47ba33594768f7
17138311 <35848872+17138311@users.noreply.github.com>
2018-11-14 13:56:53 +0000

    mark add java classes

--- ElectionApplication/src/electionapplication/PollGui.java
+++ ElectionApplication/src/electionapplication/PollGui.java
@@ -28,37 +28,40 @@ public class PollGui extends javax.swing.JFrame {
     private void initComponents() {
 
         endUserPnl = new javax.swing.JPanel();
-        can5Btn = new javax.swing.JRadioButton();
         titleLbl = new javax.swing.JLabel();
         can1Btn = new javax.swing.JRadioButton();
         can2Btn = new javax.swing.JRadioButton();
         can3Btn = new javax.swing.JRadioButton();
-        can6Btn = new javax.swing.JRadioButton();
         can4Btn = new javax.swing.JRadioButton();
         genderLbl = new javax.swing.JLabel();
-        ageLbl = new javax.swing.JLabel();
         genderCbo = new javax.swing.JComboBox<>();
-        ageCbo = new javax.swing.JComboBox<>();
+        voteCbo = new javax.swing.JComboBox<>();
+        voteLbl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        can5Btn.setText("Gavin Duffy");
+        endUserPnl.setPreferredSize(new java.awt.Dimension(500, 550));
 
+        titleLbl.setFont(new java.awt.Font("Bookman Old Style", 3, 48)); // NOI18N
         titleLbl.setText("Election 2018 Exit Poll");
 
-        can1Btn.setText("Michael D Higgins");
+        can1Btn.setBackground(new java.awt.Color(204, 204, 204));
+        can1Btn.setFont(new java.awt.Font("OCR A Extended", 0, 24)); // NOI18N
+        can1Btn.setText("18 - 25");
 
-        can2Btn.setText("Peter Casey");
+        can2Btn.setBackground(new java.awt.Color(204, 204, 204));
+        can2Btn.setFont(new java.awt.Font("OCR A Extended", 0, 24)); // NOI18N
+        can2Btn.setText("25 - 40");
 
-        can3Btn.setText("Sean Gallagher");
+        can3Btn.setBackground(new java.awt.Color(204, 204, 204));
+        can3Btn.setFont(new java.awt.Font("OCR A Extended", 0, 24)); // NOI18N
+        can3Btn.setText("40 - 65");
 
-        can6Btn.setText("Liadh Ni Riada");
-
-        can4Btn.setText("Joan Freeman");
+        can4Btn.setBackground(new java.awt.Color(204, 204, 204));
+        can4Btn.setFont(new java.awt.Font("OCR A Extended", 0, 24)); // NOI18N
+        can4Btn.setText("> 65");
 
         genderLbl.setText("Gender:");
-
-        ageLbl.setText("Age Group:");
 
         genderCbo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "", "Male", "Female" }));
         genderCbo.addActionListener(new java.awt.event.ActionListener() {
@@ -67,61 +70,62 @@ public class PollGui extends javax.swing.JFrame {
             }
         });
 
-        ageCbo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "", "18 - 25", "25 - 40", "40 - 65", "> 65" }));
+        voteCbo.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        voteCbo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "", "Michael D Higgins", "Peter Casey", "Sean Gallagher", "Joan Freeman", "Gavin Duffy", "Liadh Ni Riada" }));
+
+        voteLbl.setText("Vote:");
 
         javax.swing.GroupLayout endUserPnlLayout = new javax.swing.GroupLayout(endUserPnl);
         endUserPnl.setLayout(endUserPnlLayout);
         endUserPnlLayout.setHorizontalGroup(
             endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(endUserPnlLayout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(genderLbl)
+                .addGap(133, 133, 133)
+                .addComponent(genderCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, endUserPnlLayout.createSequentialGroup()
+                .addContainerGap(40, Short.MAX_VALUE)
                 .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(endUserPnlLayout.createSequentialGroup()
-                        .addGap(36, 36, 36)
-                        .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(can1Btn)
-                            .addComponent(can2Btn)
-                            .addComponent(can3Btn)
-                            .addComponent(genderLbl)
-                            .addComponent(ageLbl))
-                        .addGap(29, 29, 29)
-                        .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(can6Btn)
-                            .addComponent(can5Btn)
-                            .addComponent(can4Btn)
-                            .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(ageCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(genderCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(endUserPnlLayout.createSequentialGroup()
-                        .addGap(132, 132, 132)
-                        .addComponent(titleLbl)))
-                .addContainerGap(177, Short.MAX_VALUE))
+                        .addComponent(can1Btn)
+                        .addGap(50, 50, 50)
+                        .addComponent(can2Btn)
+                        .addGap(50, 50, 50)
+                        .addComponent(can3Btn)
+                        .addGap(50, 50, 50)
+                        .addComponent(can4Btn)
+                        .addContainerGap())
+                    .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, endUserPnlLayout.createSequentialGroup()
+                            .addComponent(voteLbl)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(voteCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(165, 165, 165))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, endUserPnlLayout.createSequentialGroup()
+                            .addComponent(titleLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 612, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(40, 40, 40)))))
         );
         endUserPnlLayout.setVerticalGroup(
             endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(endUserPnlLayout.createSequentialGroup()
-                .addGap(36, 36, 36)
                 .addComponent(titleLbl)
-                .addGap(31, 31, 31)
+                .addGap(34, 34, 34)
+                .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(voteCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(voteLbl))
+                .addGap(53, 53, 53)
                 .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(can1Btn)
-                    .addComponent(can4Btn))
-                .addGap(18, 18, 18)
-                .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(can2Btn)
-                    .addComponent(can5Btn))
-                .addGap(18, 18, 18)
-                .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(can3Btn)
-                    .addComponent(can6Btn))
-                .addGap(31, 31, 31)
+                    .addComponent(can4Btn))
+                .addGap(79, 79, 79)
                 .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(genderLbl)
                     .addComponent(genderCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(40, 40, 40)
-                .addGroup(endUserPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(ageLbl)
-                    .addComponent(ageCbo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(155, Short.MAX_VALUE))
+                .addContainerGap(215, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -130,15 +134,15 @@ public class PollGui extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(endUserPnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(159, Short.MAX_VALUE))
+                .addComponent(endUserPnl, javax.swing.GroupLayout.PREFERRED_SIZE, 705, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(27, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(endUserPnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(28, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -184,18 +188,16 @@ public class PollGui extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> ageCbo;
-    private javax.swing.JLabel ageLbl;
     private javax.swing.JRadioButton can1Btn;
     private javax.swing.JRadioButton can2Btn;
     private javax.swing.JRadioButton can3Btn;
     private javax.swing.JRadioButton can4Btn;
-    private javax.swing.JRadioButton can5Btn;
-    private javax.swing.JRadioButton can6Btn;
     private javax.swing.JPanel endUserPnl;
     private javax.swing.JComboBox<String> genderCbo;
     private javax.swing.JLabel genderLbl;
     private javax.swing.JLabel titleLbl;
+    private javax.swing.JComboBox<String> voteCbo;
+    private javax.swing.JLabel voteLbl;
     // End of variables declaration//GEN-END:variables
 
     void visible(boolean b) {
---
+++ ElectionApplication/src/electionapplication/age18to25.java
@@ -0,0 +1,15 @@
+package electionapplication;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author markk
+ */
+public class age18to25 {
+    
+}
